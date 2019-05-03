19c8b6fd0458707b2f5a2acd0707a91c45d56eca
ZwiityDev <49972937+tiwloveyou03@users.noreply.github.com>
2019-05-04 01:27:10 +0700

    change .example
    is my com  set  env.example delete .example but now  can coding and  delete
    public function getDateFormat()
    {
         return 'Y-m-d H:i:s.u';
    }
    when coding in notebook  insert this code in model

--- app/Orders.php
+++ app/Orders.php
@@ -8,8 +8,8 @@ use Carbon\Carbon;
 class Orders extends Model{
   protected $table='orders';
 
-  public function getDateFormat()
-{
-     return 'Y-m-d H:i:s.u';
-}
+//   public function getDateFormat()
+// {
+//      return 'Y-m-d H:i:s.u';
+// }
 }
