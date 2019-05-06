1d9cc04c83ec827ce4e19a57b9011188a284df3f
sra <arttioz@gmail.com>
2019-05-06 23:58:21 +0700

    add expire_at to orders table

--- database/orders.sql
+++ database/orders.sql
@@ -11,7 +11,7 @@
  Target Server Version : 100137
  File Encoding         : 65001
 
- Date: 03/05/2019 16:05:15
+ Date: 06/05/2019 23:58:04
 */
 
 SET NAMES utf8mb4;
@@ -35,13 +35,14 @@ CREATE TABLE `orders`  (
   `ship_cost` double(255, 0) NULL DEFAULT NULL,
   `product_cost` double(10, 2) NULL DEFAULT NULL,
   `slip_image_url` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT '',
+  `expire_at` datetime(6) NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP(6),
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Records of orders
 -- ----------------------------
-INSERT INTO `orders` VALUES (1, 1556516000, '025556666', NULL, '2019-04-29 12:33:42.000000', '2019-04-29 13:05:38.000000', NULL, NULL, 'TO PAY', NULL, NULL, NULL, NULL);
-INSERT INTO `orders` VALUES (2, 1556870057, '0897101988', NULL, '2019-05-03 14:54:29.000000', '2019-05-03 16:02:09.000000', NULL, NULL, 'TO CHECK', 'ไปรษณีย์-ลงทะเบียน', 35, 1000.00, 'http://localhost/senior-project/public/images/slip/1556874129.jpg');
+INSERT INTO `orders` VALUES (1, 1556516000, '025556666', NULL, '2019-04-29 12:33:42.000000', '2019-04-29 13:05:38.000000', NULL, NULL, 'TO PAY', NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `orders` VALUES (2, 1556870057, '0897101988', NULL, '2019-05-03 14:54:29.000000', '2019-05-03 16:02:09.000000', NULL, NULL, 'TO CHECK', 'ไปรษณีย์-ลงทะเบียน', 35, 1000.00, 'http://localhost/senior-project/public/images/slip/1556874129.jpg', '2019-05-07 23:32:49.000000');
 
 SET FOREIGN_KEY_CHECKS = 1;
